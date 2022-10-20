276f82a5d8b8cb5507e52389d3507e540341bd96
Kevin <86487929+tar83@users.noreply.github.com>
2022-10-20 14:33:07 -0500

    exercise 4.10

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -2,52 +2,14 @@ const mongoose = require('mongoose');
 const supertest = require('supertest');
 const app = require('../app');
 const Blog = require('../models/blog');
+const helper = require('./test_helper');
 
 const api = supertest(app);
-
-const blogs = [
-  {
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 7,
-  },
-  {
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-  },
-  {
-    title: 'Canonical string reduction',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-    likes: 12,
-  },
-  {
-    title: 'First class tests',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-    likes: 10,
-  },
-  {
-    title: 'TDD harms architecture',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-    likes: 0,
-  },
-  {
-    title: 'Type wars',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-    likes: 2,
-  },
-];
 
 beforeEach(async () => {
   await Blog.deleteMany({});
 
-  const blogObjects = blogs.map((blog) => new Blog(blog));
+  const blogObjects = helper.blogs.map((blog) => new Blog(blog));
   const promiseArray = blogObjects.map((blog) => blog.save());
   await Promise.all(promiseArray);
 });
@@ -61,12 +23,33 @@ test('blogs are returned as json', async () => {
 
 test('all blog posts are returned', async () => {
   const response = await api.get('/api/blogs');
-  expect(response.body).toHaveLength(blogs.length);
+  expect(response.body).toHaveLength(helper.blogs.length);
 });
 
 test('blog has an unique identifier property named id', async () => {
   const response = await api.get('/api/blogs');
   expect(response.body[0].id).toBeDefined();
+});
+
+test('a valid blog post can be added', async () => {
+  const newBlog = {
+    title: 'This is a test',
+    author: 'Kevin T.',
+    url: 'http://example.com/example.html',
+    likes: 5,
+  };
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/);
+
+  const response = await helper.blogsInDB();
+  const titles = response.map((r) => r.title);
+
+  expect(response).toHaveLength(helper.blogs.length + 1);
+  expect(titles).toContain('This is a test');
 });
 
 afterAll(() => {
