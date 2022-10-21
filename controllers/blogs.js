200ba16b6b90c267f092eee20356859a4ce772a8
Kevin <86487929+tar83@users.noreply.github.com>
2022-10-21 14:06:50 -0500

    exercise 4.17

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,8 +1,9 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
+const User = require('../models/user');
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
   response.json(blogs);
 });
 
@@ -14,14 +15,20 @@ blogsRouter.post('/', async (request, response) => {
     likes,
   } = request.body;
 
+  const users = await User.find({});
+  const user = users[0];
+
   const blog = new Blog({
     title,
     author,
     url,
     likes: likes || 0,
+    user: user.id,
   });
-  const result = await blog.save();
-  response.status(201).json(result);
+  const savedBlog = await blog.save();
+  user.blogs = user.blogs.concat(blog.id);
+  await user.save();
+  response.status(201).json(savedBlog);
 });
 
 blogsRouter.delete('/:id', async (request, response) => {
